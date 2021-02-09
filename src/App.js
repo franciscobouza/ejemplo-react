5955bb71eae8956ae16e569d4623d4a308693fb6
Francisco Bouza <franciscobouza@gmail.com>
2021-02-08 21:14:48 -0300

    Primer código JSX

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const App = () => {
+  const nombre = 'Rigoberta';
+  const apellido = 'Menchu';
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h2>Ejemplo</h2>
+        <h3>
+          {nombre} {apellido}
+        </h3>
       </header>
     </div>
   );
-}
+};
 
 export default App;

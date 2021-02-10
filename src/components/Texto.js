86fb4030a878160405bfab93076d8313b33798c6
Francisco Bouza <franciscobouza@gmail.com>
2021-02-10 20:33:29 -0300

    UseEffect!

--- src/components/Texto.js
+++ src/components/Texto.js
@@ -1,15 +1,31 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 const Texto = ({ nombre, pos }) => {
   const [contador, setContador] = useState(0);
 
+  useEffect(() => {
+    console.log('ARRANCANDONGA!');
+  }, []);
+
+  useEffect(() => {
+    console.log('OTA VE?!');
+  });
+
+  useEffect(() => {
+    console.log('OTA VE?! CAMBIO CONTADOR');
+  }, [contador]);
+
   const incrementar = () => setContador(contador + 1);
+
+  const decrementar = () => setContador(contador - 2);
+
   return (
     <>
       <p>
         {pos}: {nombre} (cantidad: {contador})
       </p>
-      <input type="button" value="Click" onClick={incrementar} />
+      <input type="button" value="Sumar" onClick={incrementar} />
+      <input type="button" value="Restar" onClick={decrementar} />
     </>
   );
 };

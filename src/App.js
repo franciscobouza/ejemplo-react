fcc1485e52bdd6413abde4d20d757b43aefbb354
Francisco Bouza <franciscobouza@gmail.com>
2021-02-10 19:52:49 -0300

    dejando todos los contadores separados

--- src/App.js
+++ src/App.js
@@ -1,12 +1,7 @@
-import React, { useState } from 'react';
 import './App.css';
 import Texto from './components/Texto';
 
 const App = () => {
-  const [contador, setContador] = useState(0);
-
-  const incrementar = () => setContador(contador + 1);
-
   const vecPersonas = [
     'Rigoberta Menchu',
     'Pablo Forlán',
@@ -22,10 +17,8 @@ const App = () => {
         <h2>Ejemplo</h2>
       </header>
       {vecPersonas.map((elem, index) => (
-        <Texto key={index} pos={index + 1} nombre={elem} contador={contador} />
+        <Texto key={index} pos={index + 1} nombre={elem} />
       ))}
-
-      <input type="button" value="Click" onClick={incrementar} />
     </div>
   );
 };
--- src/components/Texto.js
+++ src/components/Texto.js
@@ -1,11 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const Texto = ({ nombre, pos, contador }) => {
+const Texto = ({ nombre, pos }) => {
+  const [contador, setContador] = useState(0);
+
+  const incrementar = () => setContador(contador + 1);
   return (
     <>
       <p>
         {pos}: {nombre} (cantidad: {contador})
       </p>
+      <input type="button" value="Click" onClick={incrementar} />
     </>
   );
 };
